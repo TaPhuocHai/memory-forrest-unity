af9a8a0bec909e9e23c86e3b74fbf2a19284ff89
Ta Phuoc Hai <taphuochai@gmail.com>
2014-09-23 19:42:17 +0700

    refactor + mission model

---
+++ Assets/Scripts/Model/Mission.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MissionType {
+	Time,
+	Money,
+	UnlockExtraRound,
+	Coins,
+	UpgradeCard
+}
+
+public class Mission {
+	protected string _name;
+	protected string _description;
+	protected float  _rewardValue;
+	protected MissionType _missionType;
+
+	public string name {
+		get { return _name; }
+	}
+	public string description {
+		get { return _description; }
+	}
+	public float rewardValue {
+		get { return _rewardValue; }
+	}
+	public MissionType missionType {
+		get { return _missionType; }
+	}
+}
