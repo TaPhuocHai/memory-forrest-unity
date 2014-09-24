63d2b5cb651fd47ae158d6e8120bc5fe5a55da20
Ta Phuoc Hai <taphuochai@gmail.com>
2014-09-24 15:51:39 +0700

    reward

--- Assets/Scripts/Model/MissionReward/UnlockCardReward.cs
+++ Assets/Scripts/Model/MissionReward/UnlockCardReward.cs
@@ -1,1 +1,18 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
+public class UnlockCardReward : MissionReward
+{
+	public CardType cardType { get; private set; }
+	
+	public UnlockCardReward (CardType cardType)
+	{
+		this.cardType = cardType;
+	}
+	
+	public bool DoGetReward () 
+	{
+	}
+}
