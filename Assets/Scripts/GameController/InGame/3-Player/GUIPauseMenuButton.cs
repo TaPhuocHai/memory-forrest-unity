78a19214e743777a7513618a176af00f53cb5a02
Ta Phuoc Hai <taphuochai@gmail.com>
2014-10-10 11:07:48 +0700

    PHGame Helper

---
+++ Assets/Scripts/GameController/InGame/3-Player/GUIPauseMenuButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIPauseMenuButton : MonoBehaviour {
+
+	public GUIStyle style;
+
+	void OnGUI () 
+	{
+		if (PHGUI.Button(new Rect(10,10,this.style.normal.background.width,this.style.normal.background.height),"",style, true)) {
+			Debug.Log ("pause menu");
+		}
+	}
+}
---
+++ Assets/Scripts/PHGameUtility/PHScreen.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PHScreen : MonoBehaviour {
+
+	public float defaultWidth  = 1080;
+	public float defaultHeight = 1920; 
+
+	public void Awake ()
+	{
+		_instance = this;
+	}
+
+	#region Instance
+
+	static PHScreen _instance;
+	public static PHScreen Instance {
+		get {
+			return _instance;
+		}
+	}
+
+	#endregion
+
+	#region Properties
+
+	private float _ratioWith;
+	public float ratioWith 
+	{
+		get {
+			if (_ratioWith == 0) {
+				_ratioWith = Screen.width/defaultWidth;
+			}
+			return _ratioWith;
+		}
+	}
+
+	private float _ratioHeight;
+	public float ratioHeight 
+	{
+		get {
+			if (_ratioHeight == 0) {
+				_ratioHeight = Screen.height/defaultHeight;
+			}
+			return _ratioHeight;
+		}
+	}
+
+	private float _optimalRatio;
+	public float optimalRatio
+	{
+		get {
+			if (_optimalRatio == 0) {
+				if (this.ratioWith > this.ratioHeight) {
+					_optimalRatio = this.ratioHeight;
+				} else {
+					_optimalRatio = this.ratioWith;
+				}
+			}
+			return _optimalRatio;
+		}
+	}
+
+	#endregion
+}
