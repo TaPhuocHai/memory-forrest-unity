fcf399b67a8d9155ec02ab4e0a43046a15a334d7
Ta Phuoc Hai <taphuochai@gmail.com>
2014-10-11 11:05:20 +0700

    clean up

---
+++ Assets/PHGameUtility/Popup/PHPopup.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PHPopup : MonoBehaviour 
+{
+	public bool hideWhenInit = true;
+
+	virtual public void Hide (bool animation) {}
+	virtual public void Show (bool animation) {}
+}
