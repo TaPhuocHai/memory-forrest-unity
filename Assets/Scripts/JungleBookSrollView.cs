f8fdab02c3843fe5a3927fb562be013444d350ed
Ta Phuoc Hai <taphuochai@gmail.com>
2014-10-30 16:10:19 +0700

    jungle book scroll view

---
+++ Assets/Scripts/JungleBookSrollView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JungleBookSrollView : MonoBehaviour 
+{
+	public Transform jungleBook;
+
+	void Start () 
+	{
+
+		float ratio = 1.4f;
+		float left = 0;
+		UIScrollView scrollView = this.gameObject.GetComponent<UIScrollView> ();
+
+		for (int i = 0; i < 5; i ++) {
+			var book = Instantiate(this.jungleBook) as Transform;
+			book.parent = this.gameObject.transform;
+			UI2DSprite sprite = book.GetComponent<UI2DSprite> ();
+			sprite.transform.position = new Vector3 (0,0,0);
+			sprite.transform.localScale = new Vector3(ratio,ratio,1);
+			Debug.Log ("pos = " + sprite.width.ToString() + " " +  sprite.height.ToString());
+			sprite.SetRect(left, - (sprite.height * ratio)/2, sprite.width * ratio, sprite.height * ratio);
+
+			left += sprite.width * ratio + 10;
+		}
+
+		scrollView.ResetPosition ();
+	}
+}
