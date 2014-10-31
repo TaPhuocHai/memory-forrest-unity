403b8e8bc11e4ac898e1c8bd3647732d9c0620be
Ta Phuoc Hai <taphuochai@gmail.com>
2014-10-31 15:54:36 +0700

    add shop popup

---
+++ Assets/Scripts/Map/ShopPopup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopPopup : PHPopup 
+{
+	#region Singleton
+	public static ShopPopup Instance { get; private set;}
+	#endregion
+
+	public PHPanel panel;
+
+	void Awake () 
+	{
+		ShopPopup.Instance = this;		
+		this.Init ();
+	}
+	
+	void Update () {}
+	
+	#region Animation
+	
+	override public void Hide (float second) 
+	{
+		base.Hide (second);
+		this.panel.HideToDirection (PHPanelDirection.Top,second);
+	}
+	
+	override public void Show (float second) 
+	{
+		base.Show (second);
+		// Show panel
+		this.panel.Show (second);
+	}
+	
+	#endregion
+}
