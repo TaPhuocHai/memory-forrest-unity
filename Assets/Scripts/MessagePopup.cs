67ddf4658b76ea8cad445da290e7a73ea8362245
Ta Phuoc Hai <taphuochai@gmail.com>
2014-10-31 15:42:56 +0700

    fix message

---
+++ Assets/Scripts/MessagePopup.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessagePopup : PHPopup 
+{
+	#region Singleton
+	public static MessagePopup Instance { get; private set;}
+	#endregion
+
+	public PHPanel  panel;
+	public TextMesh messageText;
+
+	private string _message;
+	public string message {
+		get { return _message; }
+		set {
+			_message = value;
+			if (this.messageText) {
+				this.messageText.text = PHUtility.FormatStringMultiLine (value,50);
+			}
+		}
+	}
+
+	void Awake () 
+	{
+		MessagePopup.Instance = this;		
+		this.Init ();
+	}
+	
+	void Update () {}
+
+	#region Animation
+	
+	override public void Hide (float second) 
+	{
+		base.Hide (second);
+		this.panel.HideToDirection (PHPanelDirection.Top,second);
+	}
+	
+	override public void Show (float second) 
+	{
+		base.Show (second);
+		// Show panel
+		this.panel.Show (second);
+	}
+
+	#endregion
+}
