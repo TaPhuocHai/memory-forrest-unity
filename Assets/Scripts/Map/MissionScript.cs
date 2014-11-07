c99dbcf4f4792a0e501e11320c7c91dfd8982187
Ta Phuoc Hai <taphuochai@gmail.com>
2014-11-07 11:22:01 +0700

    add reward in mission file

--- Assets/Scripts/Map/MissionScript.cs
+++ Assets/Scripts/Map/MissionScript.cs
@@ -5,6 +5,7 @@ public class MissionScript : MonoBehaviour
 {
 	public TextMesh  title;
 	public TextMesh  description;
+	public TextMesh  reward;
 	public Transform thumbnail;
 	
 	private Mission _mission;
@@ -24,7 +25,18 @@ public class MissionScript : MonoBehaviour
 					string text = PHUtility.FormatStringMultiLine (mission.description.text, 32);
 					description.text = text;
 				}
-				
+				// Set reward 
+				if (reward != null) {
+					string text = PHUtility.FormatStringMultiLine ("+ " + mission.rewardMessage.text, 32);
+					reward.text = text;
+				}
+				Vector3 rewardPosition = new Vector3 ();
+				rewardPosition.x = description.transform.position.x;
+				rewardPosition.z = description.transform.position.z;
+				rewardPosition.y = description.transform.position.y - description.renderer.bounds.size.y/2 - 0.16f;
+				// Fix size
+				reward.transform.position = rewardPosition;
+
 				// Set thumbnail
 				if (thumbnail != null) {
 					SpriteRenderer spriteRender = thumbnail.GetComponent<SpriteRenderer>();
